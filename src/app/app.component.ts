c2ed15f5ffa0ffccf32d39465e0786ec443f1dc8
diegoferreirax <diegoferreirax95@gmail.com>
2020-12-21 21:03:24 -0300

    Commit inicial snake

---
+++ src/app/app.component.ts
@@ -0,0 +1,198 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  canvas;
+  ctx;
+  w;
+  h;
+  velocidadeX = 10;
+  velocidadeY = 0;
+  positionX = 0;
+  positionY = 0;
+  comeu = false;
+  nivel = 1;
+  pontos = 0;
+  pontosSalvos = 0;
+  frameRate = 50;
+  lastDirection = 'ArrowRight';
+
+  lsListaVelocidades = [];
+  oFruta = {};
+
+  ngOnInit() {
+
+    this.canvas = document.getElementById('canvas');
+    this.w = this.canvas.width;
+    this.h = this.canvas.height;
+    this.ctx = this.canvas.getContext("2d");
+
+    this.resetarJogo(this.canvas);
+    this.ctx.fillStyle = 'green';
+
+    window.setInterval(() => {
+
+      if (!this.comeu && this.lsListaVelocidades.length !== 0) {
+        var tail = this.lsListaVelocidades.shift();
+        this.ctx.clearRect(tail.positionX, tail.positionY, 10, 10);
+      } else {
+        this.comeu = false;
+      }
+
+      this.positionX += this.velocidadeX;
+      this.positionY += this.velocidadeY;
+
+      this.ctx.fillRect(this.positionX, this.positionY, 10, 10);
+
+      this.lsListaVelocidades.push({
+        positionX: this.positionX,
+        positionY: this.positionY
+      });
+
+      this.pontos = this.lsListaVelocidades.length - 1;
+      document.getElementById('contador').innerHTML = `Pontos ${this.pontos}`;
+
+      if ((this.pontos % 10) === 0 && this.pontosSalvos !== this.pontos) {
+        this.nivel += 1;
+        this.pontosSalvos = this.pontos;
+        this.frameRate -= 10;
+        this.ctx.fillStyle = this.retornaCorRandom();
+        document.getElementById('nivel').innerHTML = `Nível ${this.nivel}`;
+      } else {
+        document.getElementById('nivel').innerHTML = `Nível ${this.nivel}`;
+      }
+
+      this.validarColisao();
+
+    }, this.frameRate);
+
+
+    document.getElementById('body').addEventListener('keydown', (evento) => {
+      this.moverSnack(evento);
+    });
+
+  }
+
+
+
+
+
+
+
+
+
+  moverSnack = (evento) => {
+
+    switch (evento['code']) {
+      case 'ArrowLeft':
+        if (this.lastDirection != 'ArrowRight') {
+          this.velocidadeX = -10;
+          this.velocidadeY = 0;
+          this.lastDirection = evento['code'];
+        }
+        break;
+      case 'ArrowUp':
+        if (this.lastDirection != 'ArrowDown') {
+          this.velocidadeX = 0;
+          this.velocidadeY = -10;
+          this.lastDirection = evento['code'];
+        }
+        break;
+      case 'ArrowRight':
+        if (this.lastDirection != 'ArrowLeft') {
+          this.velocidadeX = 10;
+          this.velocidadeY = 0;
+          this.lastDirection = evento['code'];
+        }
+        break;
+      case 'ArrowDown':
+        if (this.lastDirection != 'ArrowUp') {
+          this.velocidadeX = 0;
+          this.velocidadeY = 10;
+          this.lastDirection = evento['code'];
+        }
+        break;
+      default:
+        break;
+    };
+  }
+
+  validarColisao = () => {
+
+    this.canvas = document.getElementById('canvas');
+    this.w = this.canvas.width;
+    this.h = this.canvas.height;
+
+    if (this.w - 10 < this.positionX || this.positionX < 0 || this.h - 10 < this.positionY || this.positionY < 0) {
+      this.resetarJogo(this.canvas);
+    }
+
+    if (this.positionX === this.oFruta['positionX'] && this.positionY === this.oFruta['positionY']) {
+      this.comeu = true;
+      this.regenerarFruta(this.canvas);
+    }
+
+    for (let i = 0; i < this.lsListaVelocidades.length - 1; i++) {
+      if (this.lsListaVelocidades[i].positionX == this.positionX && this.lsListaVelocidades[i].positionY == this.positionY) {
+        this.resetarJogo(this.canvas);
+      }
+    }
+  }
+
+  resetarJogo = (canvas) => {
+    this.ctx = canvas.getContext("2d");
+
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    this.lsListaVelocidades = [];
+    this.comeu = false;
+
+    this.pontos = 0;
+    this.nivel = 0;
+    this.frameRate = 100;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.velocidadeX = 10;
+    this.velocidadeY = 0;
+
+    this.regenerarFruta(canvas);
+  }
+
+  regenerarFruta = (canvas) => {
+    this.ctx = canvas.getContext("2d");
+
+    do {
+      var frutaX = this.retornaNumeroRandom(0, canvas.width);
+      var frutaY = this.retornaNumeroRandom(0, canvas.height);
+      var normalizedFrutaX = frutaX - (frutaX % 10);
+      var normalizedFrutaY = frutaY - (frutaY % 10);
+    } while (this.lsListaVelocidades.includes({ positionX: normalizedFrutaX, positionY: normalizedFrutaY }));
+
+
+    this.oFruta = {
+      positionX: normalizedFrutaX,
+      positionY: normalizedFrutaY
+    };
+
+    this.ctx.fillRect(this.oFruta['positionX'], this.oFruta['positionY'], 10, 10);
+  }
+
+  retornaNumeroRandom = (min, max) => {
+    return parseInt(Math.random() * (max - min) + min);
+  }
+
+  retornaCorRandom = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+}
